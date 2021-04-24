8315913b6852bb5d5dd0c5b225ff2cf9e90f99f7
Stephan Bourgeois <sbouge13a@gmail.com>
2021-04-24 13:03:47 -0400

    fixed joystick for real this time

--- Debug/joystick_functions.d
+++ Debug/joystick_functions.d
@@ -4,11 +4,12 @@ joystick_functions.obj: ../joystick_functions.c
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/assert.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h
+joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
 joystick_functions.obj: ../joystick_functions.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h
-joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h
 joystick_functions.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h
@@ -33,11 +34,12 @@ joystick_functions.obj: ../rotary_enc_task.h
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/assert.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
 ../joystick_functions.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h: 
-C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h: 
 C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h: 
--- joystick_functions.c
+++ joystick_functions.c
@@ -5,12 +5,19 @@
  *      Author: steph
  */
 #include <assert.h>
+#include <stdlib.h>
+
 #include "joystick_functions.h"
 #include "host_uart_task.h"
 #include "rotary_enc_task.h"
 
-static const uint32_t JS_HIGH_REF = 10000;
-static const uint32_t JS_LOW_REF  = 8000;
+
+
+static const uint32_t MIDPOINT = 1<<13;
+static const uint32_t WINDOW   = 1200;
+
+static const uint32_t JS_HIGH_REF = MIDPOINT + WINDOW;
+static const uint32_t JS_LOW_REF  = MIDPOINT - WINDOW;;
 
 static uart_msg_u js_uart_msg;
 
@@ -25,12 +32,10 @@ static js_state_e js_y_state = IDLE;
 
 void process_joystick(uint16_t x_raw, uint16_t y_raw) {
 
-    int32_t pitch_val;
-
     switch (js_x_state) {
     case IDLE :
 
-        if (x_raw > JS_HIGH_REF || x_raw < JS_LOW_REF) {
+        if ((x_raw > JS_HIGH_REF) || (x_raw < JS_LOW_REF)) {
             js_x_state = SEND;
         }
 
@@ -95,24 +100,22 @@ void process_joystick(uint16_t x_raw, uint16_t y_raw) {
 
         if (y_raw > JS_HIGH_REF) {
 
-            y_raw = y_raw - JS_HIGH_REF;
+
 
             js_uart_msg.bitfield.channel = get_channel();
             js_uart_msg.bitfield.pad_num = 0;
             js_uart_msg.bitfield.message_type = PITCH;
-            js_uart_msg.bitfield.value = y_raw;
+            js_uart_msg.bitfield.value = y_raw - WINDOW;
 
             send_to_host(js_uart_msg);
 
 
         } else if (y_raw < JS_LOW_REF) {
 
-            pitch_val = 0 - y_raw;
-
             js_uart_msg.bitfield.channel = get_channel();
             js_uart_msg.bitfield.pad_num = 0;
             js_uart_msg.bitfield.message_type = PITCH;
-            js_uart_msg.bitfield.value = pitch_val;
+            js_uart_msg.bitfield.value = y_raw + WINDOW;
 
             send_to_host(js_uart_msg);
 
@@ -126,7 +129,7 @@ void process_joystick(uint16_t x_raw, uint16_t y_raw) {
         js_uart_msg.bitfield.channel = get_channel();
         js_uart_msg.bitfield.pad_num = 0;
         js_uart_msg.bitfield.message_type = PITCH;
-        js_uart_msg.bitfield.value = 0;
+        js_uart_msg.bitfield.value = MIDPOINT;
 
         send_to_host(js_uart_msg);
 
