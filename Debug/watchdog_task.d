ac43071c67b4dbb3374060aacbfe9ea524e498b8
Stephan Bourgeois <sbouge13a@gmail.com>
2021-07-27 16:31:01 -0400

    new stuff

--- Debug/watchdog_task.d
+++ Debug/watchdog_task.d
@@ -1,5 +1,51 @@
 # FIXED
 
 watchdog_task.obj: ../watchdog_task.c
+watchdog_task.obj: ../watchdog_task.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdbool.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/FreeRTOS.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOSConfig.h
+watchdog_task.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/projdefs.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/portable.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/deprecated_definitions.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/mpu_wrappers.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/task.h
+watchdog_task.obj: C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/list.h
 
 ../watchdog_task.c: 
+../watchdog_task.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdbool.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/FreeRTOS.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOSConfig.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/projdefs.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/portable.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/deprecated_definitions.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/mpu_wrappers.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/task.h: 
+C:/Users/steph/workspace_v10/drum_pad/FreeRTOS/include/list.h: 
